0bbeff80386056b35821cc3929381a550394137b
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2014-10-12 21:23:53 +0200

    Model presentationSession ok

--- cdl/schedule/models.py
+++ cdl/schedule/models.py
@@ -7,8 +7,13 @@ from symposion.schedule.models import Presentation, Room, Day
 class PresentationSession(models.Model):
 
     presentation = models.OneToOneField(Presentation, related_name="presentation")
-    room = models.ForeignKey(Room, related_name="room")
     day = models.ForeignKey(Day, related_name="day")
     start = models.TimeField()
     end = models.TimeField()
+    room = models.ForeignKey(Room, related_name="room", null=True, blank=True)
 
+    def __unicode__(self):
+        return u"%s - %s to %s : %s" % (self.day, self.start, self.end, self.presentation, )
+
+    class Meta:
+        ordering = ["day", "start", "end"]
