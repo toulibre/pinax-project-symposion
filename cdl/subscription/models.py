68754bdc1ab21b4f690370b0cc02600e693b4741
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2014-10-26 22:25:22 +0100

    Translation

--- cdl/subscription/models.py
+++ cdl/subscription/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
+
 from symposion.schedule.models import Presentation
 
 class Subscription(models.Model):
@@ -11,10 +12,18 @@ class Subscription(models.Model):
     max_attendees = models.IntegerField()
 
     def __unicode__(self):
-        return u"Subscription to %s" % self.presentation
+        return _(u"Subscription to %s") % self.presentation
     
     def all_attendees(self):
         return self.attendees.all()
+
+    @property
+    def attendees_nb(self):
+        return self.attendees.count()
+
+    def is_closed(self):
+        """True if max attendees number is reached"""
+        return self.max_attendees <= self.attendees_nb
 
 class Attendee(models.Model):
     """Attendee to a talk or a presentation
@@ -22,9 +31,9 @@ class Attendee(models.Model):
 
     #~ user = models.OneToOneField(User, null=True, related_name="attendee_profile")
     subscribe_to = models.ForeignKey('Subscription', related_name="attendees")
-    email = models.EmailField(help_text="Used in case of cancelling")
+    email = models.EmailField(help_text=_(u"Used in case of cancelling"))
     name = models.CharField(max_length=100, help_text="Your name")
-    interests = models.TextField(help_text="Your project", null=True, blank=True)
+    interests = models.TextField(help_text=_(u"Your project"), null=True, blank=True)
 
     def __unicode__(self):
         return self.name
