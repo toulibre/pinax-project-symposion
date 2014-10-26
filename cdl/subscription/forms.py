d3a26e4a0b4f40a3742aa09e9a49b0e2267307e4
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2014-10-26 22:46:34 +0100

    Add captcha

--- cdl/subscription/forms.py
+++ cdl/subscription/forms.py
@@ -1,12 +1,15 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from markitup.widgets import MarkItUpWidget
 
 from cdl.subscription.models import Attendee
-
+from cdl.subscription.utils import validate_3_plus_4
 
 class AttendeeForm(forms.ModelForm):
-    
+
+    captcha = forms.IntegerField(help_text=_(u"How much 3 + 4 makes?"), validators=[validate_3_plus_4])
+
     class Meta:
         model = Attendee
         fields = [
