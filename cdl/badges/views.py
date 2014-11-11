3b89787a2182cfbb0b222ac7aacc2ba9ffdae27b
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2014-11-11 12:15:34 +0100

    Fix cospeakers not in badges

--- cdl/badges/views.py
+++ cdl/badges/views.py
@@ -2,6 +2,7 @@
 
 from django.http import Http404, HttpResponse
 from django.template import loader, Context
+from django.db.models import Q
 
 from django.contrib.auth.decorators import login_required
 from symposion.speakers.models import Speaker
@@ -17,21 +18,22 @@ def badges_list_csv(request):
     benevoles = Membership.objects.filter(team__slug="benevoles").order_by("user__first_name")
     for benevole in benevoles:
         attendee = {}
+        attendee['organisation'] = u" "
         if hasattr(benevole.user,'speaker_profile'):
             attendee['name'] = benevole.user.speaker_profile.name
+            attendee['organisation'] = benevole.user.speaker_profile.organisation
         else:
             attendee['name'] = u" ".join([benevole.user.first_name, benevole.user.last_name])
-        attendee['organisation'] = u" "
         attendee['function'] = u"Bénévole"
         if benevole.user.is_staff:
             attendee['function'] = u" / ".join([u"Organisation", attendee['function']])
         attendees.append(attendee)
 
-    speakers = Speaker.objects.exclude(presentations__isnull=True).order_by("name")
+    speakers = Speaker.objects.filter(~Q(copresentations__isnull=True) | ~Q(presentations__isnull=True)).order_by("name")
     for speaker in speakers:
         attendee = {}
         attendee['name'] = speaker.name
-        attendee['organisation'] = u" "
+        attendee['organisation'] = u""
         if hasattr(speaker, 'organisation'):
             attendee['organisation'] = speaker.organisation
         attendee['function'] = u"Orateur"
