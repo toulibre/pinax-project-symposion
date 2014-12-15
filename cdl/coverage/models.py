871385db0edeb719817e1dab6b0180bf3fa95c94
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2014-12-16 00:23:07 +0100

    Video and audio extension calcul

--- cdl/coverage/models.py
+++ cdl/coverage/models.py
@@ -1,3 +1,6 @@
+import os
+import urlparse
+
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -13,6 +16,13 @@ COVERAGE_TYPES = (
     ('slides', _(u"Slides")),
     ('transcription', _(u"Transcription")),
 )
+
+MEDIA_EXTENSION = ['mp4', 'webm', 'ogv', 'ogg', 'mp3', 'flac']
+
+def get_extension(url):
+    """get extension from url"""
+    path = urlparse.urlparse(url).path
+    return os.path.splitext(path)[1][1:]
 
 
 class Licence(models.Model):
@@ -41,9 +51,26 @@ class Coverage(models.Model):
     coverage_type = models.CharField(max_length=30, choices=COVERAGE_TYPES, verbose_name=_(u"Coverage type"))
     licence = models.ForeignKey(Licence, verbose_name=_(u"Coverage licence"),)
     url = models.URLField(blank=True)
-    url2 = models.URLField(blank=True)
+    url2 = models.URLField(blank=True, help_text=_(u"Url to another format for audio or video media."))
     poster = models.URLField(blank=True)
     coverage_file = models.FileField(upload_to=uuid_filename, verbose_name=_(u"Coverage file"), blank=True)
 
     def __unicode__(self):
         return u"%s - %s" % (self.title, self.presentation)
+
+    def url_extension(self):
+        """return url extension if exists
+        """
+        if hasattr(self, 'url'):
+            extension = get_extension(self.url)
+            if extension != '' and extension in MEDIA_EXTENSION:
+                return extension
+
+    def url2_extension(self):
+        """return url extension if exists
+        """
+        if hasattr(self, 'url'):
+            extension = get_extension(self.url2)
+            if extension != '' and extension in MEDIA_EXTENSION:
+                return extension
+
